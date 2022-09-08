8bf19cf1b2b12dd4a592e61f87a7b61b1e8d00a5
den1sR <112901510+den1sR@users.noreply.github.com>
2022-09-09 00:07:26 +0300

    Add files via upload

---
+++ src/app/network-metrices/network-metrices.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Nodes } from '../nodes';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+
+@Component({
+  selector: 'app-network-metrices',
+  templateUrl: './network-metrices.component.html',
+  styleUrls: ['./network-metrices.component.css']
+})
+export class NetworkMetricesComponent implements OnInit {
+
+
+
+  jsStr: any;
+  lenNodes: any;
+  jsSt: [] = [];
+  edgesObj: [] = [];
+  nodesNum: string;
+  blya: any;
+  strArr: any;
+  bom: any;
+fwl:any;
+
+  constructor(private _route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+
+    this.jsStr = history.state;
+    this.blya = this.jsStr.data.Edges.filter(element => element.Source === "100094");
+this.fwl = this.jsStr.data.Edges.filter(element => element.Source === element.Target);
+    this.lenNodes = JSON.parse(JSON.stringify(this.jsStr.data.Nodes));
+    this.edgesObj = JSON.parse(JSON.stringify(this.jsStr.data));
+    this.nodesNum = this.lenNodes.length;
+    console.log(this.edgesObj);
+    this.strArr = JSON.stringify(this.jsStr.data.Edges);
+
+    // console.log(this.jsStr.data.Nodes.length);
+    console.log("this is obt: " + this.blya);
+    console.log(this.lenNodes);
+  }
+
+}
---
+++ src/app/nodes.ts
@@ -0,0 +1,4 @@
+export interface Nodes {
+    id:number,
+    Label:string
+}
